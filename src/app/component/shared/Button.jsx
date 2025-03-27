1cb7cb389826a55bfdfb4aae8d6c2dc355137860
Saif Hasan <saifhassan399@gmail.com>
2025-03-27 19:35:31 +0600

    first commit

---
+++ src/app/component/shared/Button.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Button = ({ title, href = '#', padding = 'default', secondary = false }) => {
+  const paddingClasses = {
+    small: 'px-4',
+    default: 'px-6',
+    large: 'px-8',
+    xlarge: 'px-12',
+    xxlarge: 'px-[80px]',
+  };
+
+  const paddingClass = paddingClasses[padding] || paddingClasses.default;
+
+  return (
+    <button
+      className={`${
+        secondary ? 'bg-secondary border-secondary' : 'bg-primary border-primary'
+      }  border  font-medium text-lg text-[#FCFCFD] ${paddingClass} py-3 rounded-[40px]`}
+    >
+      <Link className="w-full" href={href}>
+        {title}
+      </Link>
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/app/our-thinking/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OurThinking = () => {
+  return <div>OurThinking</div>;
+};
+
+export default OurThinking;
