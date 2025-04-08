a5514b6d16027e365885bd76b7bcb68b438424a0
Saif Hasan <saifhassan399@gmail.com>
2025-04-08 17:35:43 +0600

    Update - New notfication will now show at the top

---
+++ src/app/component/home/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '../shared/Button';
+import headerBanner from '/public/AdobeStock_604839762.jpg';
+
+const Header = () => {
+  return (
+    <header
+      style={{
+        backgroundImage: `url('/AdobeStock_604839762.jpg'), linear-gradient(to right, rgba(62, 0, 101, 0.90) 15.68%, rgba(62, 0, 101, 0.45) 69.4%)`,
+        backgroundBlendMode: 'overlay',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+      className="pt-[187px] relative md:pt-[238px] pb-[176px] md:pb-[203px] px-5 md:px-[60px]"
+    >
+      <div className="w-full relative z-10 lg:max-w-[80%]">
+        <h1 className="font-semibold tracking-[-4%] leading-[120%] text-5xl md:[65px] lg:text-[84px] text-white">
+          Empowering Teams with Expert People Skills Training
+        </h1>
+        <p className="text-white mt-[18px] md:mt-6 mb-[26px] md:mb-8 w-[70%] font-normal text-sm md:text-lg">
+          Discover tailored, experiential training solutions that are meticulously designed to empower leaders, enhance individual growth,
+          and foster collaboration.
+        </p>
+        <div className="flex md:flex-row flex-col items-center gap-4">
+          <Button title={'Book a Free Consultation'} href="#" />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
