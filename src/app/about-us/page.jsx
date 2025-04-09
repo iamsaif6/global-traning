4cacef069e42df1d90a1e67eec9bda563ba6dcb7
Saif Hasan <saifhassan399@gmail.com>
2025-04-09 18:25:33 +0600

    Update - tranning page added

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -10,7 +10,7 @@ const AboutUS = () => {
     <div>
       <header
         style={{
-          backgroundImage: `url('/AdobeStock_604839762.jpg'), linear-gradient(to right, #350b50 0%, rgba(62, 0, 101, 0.20) 100%)`,
+          backgroundImage: `linear-gradient(to right, #350b50 0%, rgba(62, 0, 101, 0.20) 100%)`,
           backgroundBlendMode: 'overlay',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
@@ -19,11 +19,9 @@ const AboutUS = () => {
       >
         <div className="w-full relative z-10 lg:max-w-[80%]">
           <h1 className="font-semibold tracking-[-4%] leading-[120%] text-5xl md:[65px] lg:text-[68px] text-white">
-            About Global Training
-          </h1>
-          <p className="text-white mt-[18px] md:mt-6 mb-[26px] md:mb-8 w-[70%] font-normal text-sm md:text-lg">
             Real learning starts with real people
-          </p>
+          </h1>
+          <p className="text-white mt-[18px] md:mt-6 mb-[26px] md:mb-8 w-[70%] font-normal text-sm md:text-lg">About Global Training</p>
         </div>
       </header>
       <WhoWeAre />
