f102f04c5b37c6491781c140f29299103ba376a0
Saif Hasan <saifhassan399@gmail.com>
2025-04-09 17:41:24 +0600

    Update - tranning page added

--- src/app/component/about-us/Impact.jsx
+++ src/app/component/about-us/Impact.jsx
@@ -7,21 +7,44 @@ const Impact = () => {
     <section className="py-[120px] bg-primary  items-center px-[40px] grid grid-cols-2 gap-12">
       <div>
         <div className={`w-full text-white  max-w-[660px]`}>
-          <p className="mb-3 text-white font-medium">OUR PHILOSOPHY</p>
-          <h1 className="  font-medium text-[56px] leading-[120%] -tracking-[1.6px]">Compassion at the Heart of Learning</h1>
+          <p className="mb-3 text-white font-medium">THE GLOBAL TRAINING IMPACT</p>
+          <h1 className="  font-medium text-[46px] leading-[120%] -tracking-[1.6px]">
+            Empowering People, Strengthening Teams, Driving Change
+          </h1>
 
-          <p className=" mt-10 mb-[56px]  text-lg font-normal leading-[140%]">
-            When people feel seen, safe and supported, they open up — to ideas, to challenge, to each other.
-            <br /> <br />
-            That is when true learning begins.
-            <br /> <br />
-            Our role is to create the space for that transformation.
-            <br /> <br />
-            By combining emotional intelligence with experiential practice, we help individuals and teams strengthen connection, build
-            confidence and unlock potential.
-            <br /> <br />
-            Because when people grow together, they go further together.
-          </p>
+          <p className=" mt-10 mb-[56px]  text-lg font-normal leading-[140%]">Our programmes deliver tangible change:</p>
+
+          <div className="space-y-4">
+            <div className="bg-white p-6 rounded-3xl flex gap-6">
+              <span className="w-10 h-10 text-white font-medium text-2xl flex items-center justify-center bg-secondary rounded-[8px]">
+                1
+              </span>
+              <div>
+                <h3 className="text-[28px] text-[#18181B] font-medium mb-2">Organization</h3>
+                <p className="text-[#6E7381] text-base">Organisations see stronger collaboration and culture</p>
+              </div>
+            </div>
+
+            <div className="bg-white p-6 rounded-3xl flex gap-6">
+              <span className="w-10 h-10 text-white font-medium text-2xl flex items-center justify-center bg-secondary rounded-[8px]">
+                2
+              </span>
+              <div>
+                <h3 className="text-[28px] text-[#18181B] font-medium mb-2">Manager</h3>
+                <p className="text-[#6E7381] text-base">Managers lead with greater clarity and care</p>
+              </div>
+            </div>
+
+            <div className="bg-white p-6 rounded-3xl flex gap-6">
+              <span className="w-10 h-10 text-white font-medium text-2xl flex items-center justify-center bg-secondary rounded-[8px]">
+                3
+              </span>
+              <div>
+                <h3 className="text-[28px] text-[#18181B] font-medium mb-2">Employee</h3>
+                <p className="text-[#6E7381] text-base">Employees feel more connected, capable and confident</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="max-h-[680px] h-full rounded-3xl overflow-hidden">
