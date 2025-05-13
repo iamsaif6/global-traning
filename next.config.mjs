72ba5454dfa9100be00bc02af10fdbc5955d2e0b
Saif Hasan <saifhassan399@gmail.com>
2025-05-13 18:11:52 +0600

    Service Training page added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      'localhost',
+      'complete-addition-236de36f0b.media.strapiapp.com',
+      'trusty-nature-dfe65baab6.media.strapiapp.com',
+      'smart-feast-4ad402f782.media.strapiapp.com',
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import img1 from '/public/image 12.png';
 import Image from 'next/image';
 import Button from '../component/shared/Button';
+import { fetchArticles } from '@/utils/api';
+import Link from 'next/link';
 
-const page = () => {
+const page = async () => {
+  const articles = await fetchArticles();
+
   return (
     <div>
       <header
@@ -28,7 +32,7 @@ const page = () => {
       {/* Blogs */}
       <section className="pb-[80px] lg:pb-[120px] px-5 lg:px-[40px]">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => {
+          {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(item => {
             return (
               <div className=" rounded-3xl pb-8 mb-[35px]   overflow-hidden">
                 <Image src={img1} className="h-[300px] rounded-b-3xl w-full" />
@@ -38,6 +42,37 @@ const page = () => {
                   <button className="px-6 mt-6 py-3 rounded-[40px] text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer">
                     Read now
                   </button>
+                </div>
+              </div>
+            );
+          })} */}
+          {articles.data.map(item => {
+            return (
+              <div key={item.name} className=" rounded-3xl pb-8 mb-[35px]   overflow-hidden">
+                <Image
+                  alt={item.name || 'Cover'}
+                  width={200}
+                  height={200}
+                  src={item?.cover?.url || img1}
+                  className="h-[300px] rounded-b-3xl w-full"
+                />
+                <div className="mt-8 px-6">
+                  <p className="text-secondary font-normal text-sm">
+                    {new Date(item.publishedAt).toLocaleDateString('en-US', {
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                  </p>
+                  <h3 title={item.title} className="text-[#18181B] truncate font-medium text-2xl mt-3">
+                    {item.title}
+                  </h3>
+                  <Link
+                    href={`/blogs/${item?.slug}`}
+                    className="px-6 mt-6 py-3 rounded-[40px] inline-block text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer"
+                  >
+                    Read now
+                  </Link>
                 </div>
               </div>
             );
