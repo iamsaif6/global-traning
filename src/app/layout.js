ee6cee98cb0f8d47b1ee13dfbe7c07267ef5b1f9
Saif Hasan <saifhassan399@gmail.com>
2025-05-13 09:19:34 +0600

    Botpress added

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Poppins } from 'next/font/google';
 import './globals.css';
 import Navbar from './component/layout/Navbar';
 import Footer from './component/layout/Footer';
+import Script from 'next/script';
 
 const poppins = Poppins({
   variable: '--font-poppins',
@@ -18,6 +19,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
+        <Script src="https://cdn.botpress.cloud/webchat/v2.3/inject.js" strategy="afterInteractive" />
+        <Script src="https://files.bpcontent.cloud/2025/04/20/21/20250420213141-RFCIFB7R.js" strategy="afterInteractive" />
         <Navbar />
         {children}
         <Footer />
