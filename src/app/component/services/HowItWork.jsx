c83cf4adb54b4f886f415f88a36d06b6a1970f14
Saif Hasan <saifhassan399@gmail.com>
2025-05-16 15:01:07 +0600

    Contact page ui bug fixed

--- src/app/component/services/HowItWork.jsx
+++ src/app/component/services/HowItWork.jsx
@@ -21,11 +21,11 @@ const HowItWork = () => {
       {/* Grid */}
       <div className="max-w-[1130px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 lg:mt-[80px]">
         <div className=" rounded-3xl bg-white p-8">
-          <p className="mb-8">
+          <div className="mb-8">
             <div className=" duration-200 bg-secondary text-white  lg:w-[40px] lg:h-[40px] w-[36px] h-[36px] flex-shrink-0 rounded-[8px] flex items-center justify-center font-medium text-[28px]">
               1
             </div>
-          </p>
+          </div>
           <p className="text-[#511A75] font-medium text-[28px] lg:text-[32px] leading-[120%] mb-3">Book a discovery call</p>
           <p className="text-[#6E7381] text-base lg:text-lg font-normal leading-[140%]">
             We’ll talk through what’s going on, the challenges, dynamics or shifts you’re hoping for.
@@ -36,11 +36,11 @@ const HowItWork = () => {
         </div>
 
         <div className=" rounded-3xl bg-white p-8">
-          <p className="mb-8">
+          <div className="mb-8">
             <div className=" duration-200 bg-secondary text-white  lg:w-[40px] lg:h-[40px] w-[36px] h-[36px] flex-shrink-0 rounded-[8px] flex items-center justify-center font-medium text-[28px]">
               2
             </div>
-          </p>
+          </div>
           <p className="text-[#511A75] font-medium text-[28px] lg:text-[32px] leading-[120%] mb-3">We listen, explore and ask questions</p>
           <p className="text-[#6E7381] text-base lg:text-lg font-normal leading-[140%]">
             So we can fully understand the context, what’s at stake, and what kind of change would feel real and lasting.
@@ -52,11 +52,11 @@ const HowItWork = () => {
         </div>
 
         <div className=" rounded-3xl bg-white p-8">
-          <p className="mb-8">
+          <div className="mb-8">
             <div className=" duration-200 bg-secondary text-white  lg:w-[40px] lg:h-[40px] w-[36px] h-[36px] flex-shrink-0 rounded-[8px] flex items-center justify-center font-medium text-[28px]">
               3
             </div>
-          </p>
+          </div>
           <p className="text-[#511A75] font-medium text-[28px] lg:text-[32px] leading-[120%] mb-3">
             We propose a clear, thoughtful solution
           </p>
@@ -70,11 +70,11 @@ const HowItWork = () => {
         </div>
 
         <div className=" rounded-3xl bg-white p-8">
-          <p className="mb-8">
+          <div className="mb-8">
             <div className=" duration-200 bg-secondary text-white  lg:w-[40px] lg:h-[40px] w-[36px] h-[36px] flex-shrink-0 rounded-[8px] flex items-center justify-center font-medium text-[28px]">
               4
             </div>
-          </p>
+          </div>
           <p className="text-[#511A75] font-medium text-[28px] lg:text-[32px] leading-[120%] mb-3">Together, we refine it</p>
           <p className="text-[#6E7381] text-base lg:text-lg font-normal leading-[140%]">
             Until it fits your people, your timing and your goals.
@@ -85,11 +85,11 @@ const HowItWork = () => {
         </div>
 
         <div className=" rounded-3xl bg-white p-8">
-          <p className="mb-8">
+          <div className="mb-8">
             <div className=" duration-200 bg-secondary text-white  lg:w-[40px] lg:h-[40px] w-[36px] h-[36px] flex-shrink-0 rounded-[8px] flex items-center justify-center font-medium text-[28px]">
               5
             </div>
-          </p>
+          </div>
           <p className="text-[#511A75] font-medium text-[28px] lg:text-[32px] leading-[120%] mb-3">We deliver</p>
           <p className="text-[#6E7381] text-base lg:text-lg font-normal leading-[140%]">
             We deliver with care, creativity and skill, in a way that brings people with us.
