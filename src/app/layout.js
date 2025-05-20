a7af381c97d3c684ab2778e4cf7491d249b946d3
Saif Hasan <saifhassan399@gmail.com>
2025-05-21 00:50:34 +0600

    Contact page ui bug fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,9 +3,7 @@ import './globals.css';
 import Navbar from './component/layout/Navbar';
 import Footer from './component/layout/Footer';
 import Script from 'next/script';
-import UserbackComponent from '@/component/UserbackComponent';
-import FeedbackButton from '@/component/FeedbackButton';
-
+import UserbackWidget from '@/component/UserbackWidget';
 const poppins = Poppins({
   variable: '--font-poppins',
   subsets: ['latin'],
@@ -21,12 +19,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased`}>
-        <FeedbackButton />
+        <UserbackWidget />
         <Script src="https://cdn.botpress.cloud/webchat/v2.4/inject.js" strategy="afterInteractive"></Script>
         <Script src="https://files.bpcontent.cloud/2025/05/20/09/20250520093739-YCHB89K8.js" strategy="afterInteractive"></Script>
         <Navbar />
         {children}
-        <UserbackComponent apiKey="A-toB4qf6TlycGzt55mrEgeMRHe" />
         <Footer />
       </body>
     </html>
