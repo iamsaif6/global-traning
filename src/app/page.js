2138376a1272080b8f9bdaa3c4a00a534783fc1e
Saif Hasan <saifhassan399@gmail.com>
2025-06-02 14:44:18 +0600

    Team CTA removed

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
       <WhatWeDo />
       <Impact />
       <WhyUs />
-      <OurTeam title={'Meet Our Team'} button />
+      <OurTeam title={'Meet Our Team'} />
       {/* <Instructor /> */}
       <HowItWork />
       <Blogs />
