a9476108e9c63a78ea973cb6837ed75e74217bdd
Saif Hasan <saifhassan399@gmail.com>
2025-07-23 12:58:37 +0600

    related case studies updated

--- src/component/case-studies/Blog.jsx
+++ src/component/case-studies/Blog.jsx
@@ -3,9 +3,10 @@ import Image from 'next/image';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import img1 from '/public/image 12.png';
-import img2 from '/public/image 50.png';
-import img3 from '/public/image 20.png';
+import Link from 'next/link';
+import trip from '/public/Trip.png';
+import uniliver from '/public/unilever_logo.png';
+import global from '/public/GM logo 1.png';
 
 const Blog = () => {
   const settings = {
@@ -49,35 +50,59 @@ const Blog = () => {
       <div className="slider-container mt-[56px]">
         <Slider {...settings}>
           <div className=" rounded-3xl px-3 pb-8   overflow-hidden">
-            <Image alt="What Are Courageous Conversations in the Workplace?" src={img1} className="h-[300px]  rounded-3xl w-full" />
+            <div className="h-[300px]">
+              <Image
+                alt="What Are Courageous Conversations in the Workplace?"
+                src={trip}
+                className="h-full max-w-[200px] mx-auto object-contain rounded-3xl w-full"
+              />
+            </div>
             <div className="mt-8 px-6">
               <p className="text-secondary font-normal text-sm">December 4, 2024</p>
-              <h3 className="text-[#18181B] font-medium text-2xl mt-3">Transforming Leadership at NHS</h3>
-              <button className="px-6 mt-6 py-3 rounded-[40px] text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer">
-                Read now
-              </button>
+              <h3 className="text-[#18181B] font-medium text-2xl mt-3">Communication Training For TripAdvisor</h3>
+              <Link href={'/case-studies-1'}>
+                <button className="px-6 mt-6 py-3 rounded-[40px] text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer">
+                  Read now
+                </button>
+              </Link>
             </div>
           </div>
 
           <div className=" rounded-3xl px-3 pb-8   overflow-hidden">
-            <Image alt="What Are Courageous Conversations in the Workplace?" src={img2} className="h-[300px]  rounded-3xl w-full" />
+            <div className="h-[300px]">
+              <Image
+                alt="What Are Courageous Conversations in the Workplace?"
+                src={uniliver}
+                className="h-full max-w-[200px] mx-auto object-contain rounded-3xl w-full"
+              />
+            </div>
             <div className="mt-8 px-6">
               <p className="text-secondary font-normal text-sm">December 4, 2024</p>
-              <h3 className="text-[#18181B] font-medium text-2xl mt-3">Transforming Leadership at NHS</h3>
-              <button className="px-6 mt-6 py-3 rounded-[40px] text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer">
-                Read now
-              </button>
+              <h3 className="text-[#18181B] font-medium text-2xl mt-3">Well-Being Workshop For Unilever</h3>
+              <Link href={'/case-studies-1'}>
+                <button className="px-6 mt-6 py-3 rounded-[40px] text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer">
+                  Read now
+                </button>
+              </Link>
             </div>
           </div>
 
           <div className=" rounded-3xl px-3 pb-8   overflow-hidden">
-            <Image alt="What Are Courageous Conversations in the Workplace?" src={img3} className="h-[300px]  rounded-3xl w-full" />
+            <div className="h-[300px]">
+              <Image
+                alt="What Are Courageous Conversations in the Workplace?"
+                src={global}
+                className="h-full max-w-[200px] mx-auto object-contain rounded-3xl w-full"
+              />
+            </div>
             <div className="mt-8 px-6">
               <p className="text-secondary font-normal text-sm">December 4, 2024</p>
-              <h3 className="text-[#18181B] font-medium text-2xl mt-3">Transforming Leadership at NHS</h3>
-              <button className="px-6 mt-6 py-3 rounded-[40px] text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer">
-                Read now
-              </button>
+              <h3 className="text-[#18181B] font-medium text-2xl mt-3">Mediation Training For Global Mediation</h3>
+              <Link href={'/case-studies-1'}>
+                <button className="px-6 mt-6 py-3 rounded-[40px] text-lg font-medium  hover:bg-primary hover:text-white duration-200 border border-[#D1D1D6] hover:border-primary cursor-pointer">
+                  Read now
+                </button>
+              </Link>
             </div>
           </div>
         </Slider>
