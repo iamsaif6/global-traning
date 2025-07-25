0f8c54978afc8068846e5f8688971561fbabed62
Saif Hasan <saifhassan399@gmail.com>
2025-07-25 20:01:46 +0600

    Footer bg updated

--- src/app/component/home/Header.jsx
+++ src/app/component/home/Header.jsx
@@ -13,20 +13,22 @@ const Header = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'top',
       }}
-      className="py-[150px] h-auto md:h-screen relative  md:pt-[208px] px-5 md:px-[60px]"
+      className="pt-[187px] max-h-[1050px]  h-screen custom-gradient-bg bg-[center] sm:bg-[center_top] lg:bg-bottom max-sm:bg-[65%_center] relative md:pt-[238px] flex items-center pb-[176px] md:pb-[203px] px-5 md:px-[60px]"
     >
-      <div className="w-full relative z-10  mx-auto text-center">
-        <h1 className="font-medium  tracking-[-4%] leading-[120%] text-5xl  lg:text-[68px] !text-[#FCFCFD]">
-          Every voice matters.
-          <br />
-          every story counts.
-        </h1>
-        <p className="text-[#FCFCFD] text-left lg:text-center mx-auto  mt-[18px] md:mt-6 mb-[26px] md:mb-8 w-full lg:w-[70%] font-normal text-base md:text-lg">
-          We design and deliver practical, immersive and human-centered training experiences that strengthen how people relate, reflect and
-          collaborate. Empowering people to grow, connect and lead with emotional intelligence.
-        </p>
-        <div onClick={() => setOpen(true)} className="flex max-w-max mx-auto gap-4 justify-center">
-          <Button href="/contact-us" title={'Start a conversation'} />
+      <div className="w-full  flex items-center  text-left lg:pl-5 relative z-10 lg:max-w-[90vw] mx-auto">
+        <div className="w-full relative z-10  mx-auto text-center">
+          <h1 className="font-semibold text-center mx-auto tracking-[-4%] max-w-[900px] 2xl:max-w-[90%] leading-[120%] text-5xl md:text-[65px] xl:text-[4vw] !text-white ">
+            Every voice matters.
+            <br />
+            every story counts.
+          </h1>
+          <p className="text-white mt-[25px] mx-auto md:mt-4 mb-[25px] mr-auto max-w-[900px] md:mb-10 w-[100%] font-normal text-base md:text-lg xl:text-xl">
+            We design and deliver practical, immersive and human-centered training experiences that strengthen how people relate, reflect
+            and collaborate. Empowering people to grow, connect and lead with emotional intelligence.
+          </p>
+          <div onClick={() => setOpen(true)} className="flex max-w-max mx-auto gap-4 justify-center">
+            <Button href="/contact-us" title={'Start a conversation'} />
+          </div>
         </div>
       </div>
       {/* <Popup setOpen={setOpen} isOpen={open} /> */}
