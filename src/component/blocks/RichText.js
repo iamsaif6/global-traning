6c688cd0e238f75bca5a37cfaa72577fbb9a3822
Saif Hasan <saifhassan399@gmail.com>
2025-07-28 20:03:35 +0600

    Strapi api updated

--- src/component/blocks/RichText.js
+++ src/component/blocks/RichText.js
@@ -4,8 +4,16 @@ import rehypeRaw from 'rehype-raw';
 
 export function RichText({ content }) {
   return (
-    <div className="md:prose-xl leading-[160%] prose max-w-full">
-      <ReactMarkdown rehypePlugins={[rehypeRaw]}>{content}</ReactMarkdown>
+    <div className="prose prose-xl max-w-full leading-[160%]">
+      <ReactMarkdown
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          h1: ({ node, ...props }) => <h1 className="text-4xl font-bold mt-6 mb-4" {...props} />,
+          h2: ({ node, ...props }) => <h2 className="text-3xl font-semibold mt-4 mb-3" {...props} />,
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     </div>
   );
 }
